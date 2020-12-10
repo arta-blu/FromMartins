089e04ec6361e6a3b3740083a003f0f8b8d93337
AigaRozentale <aiga.rozentaale@gmail.com>
2020-12-10 21:18:05 +0200

    Submit or search

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
 
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// cookbook project
+
+
+        System.out.print("Hello user! Choose if You want to submit (press 1) or search for recipe (press 2)!");
+        Scanner input = new Scanner(System.in);
+        String userChoice = input.nextLine();
+
+
+
     }
-    int x = 0;
-    int y = 0;
+
 }
